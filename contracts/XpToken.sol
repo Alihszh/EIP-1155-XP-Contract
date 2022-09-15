053c58d29c43a73148828bd0cc7c71b76a7fabdd
alihsz <57255882+alihsz@users.noreply.github.com>
2022-09-15 13:29:16 +0430

    initial commit

--- contracts/XpToken.sol
+++ contracts/XpToken.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "./nft_explorer.sol";
 
 contract XpToken is Ownable, ERC1155, ERC1155Burnable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _counter;
     uint256 public constant XP = 0;
     address public _accountZero = 0x55f58b0241728459aC1F26613d4EE6D439A9e7A2; //wallet which tokens dump at
     address public _nftExplorerAddress; //nft_explorer address
@@ -14,16 +18,22 @@ contract XpToken is Ownable, ERC1155, ERC1155Burnable {
     constructor() ERC1155("") {}
 
     struct info {
-        uint256 xp;
         address from;
         uint16 G_code;
         uint16 B_code;
+        uint256 xp;
     }
 
     mapping(uint256 => info) public history;
 
-    function assign_xp(address to, uint256 amount) public onlyOwner {
-        _mint(to, XP, amount, "");
+    function assign_xp(address to, uint256 amount, uint16 g_code) public onlyOwner {
+        if(_mint(to, XP, amount, "")){
+            history[_counter.current()].xp = amount;
+            history[_counter.current()].from = to;
+            history[_counter.current()].G_code = g_code;
+        }
+
+
     }
 
     function assign_xp_toNFT(
